e5cec9789399d2b1dcba463b5bc81b3296deb0b0
Leoberto Soares <leobertosoares@lavid.ufpb.br>
2020-09-20 10:46:17 -0300

    improving run scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker rm v4h-example -f
 docker build -t v4h-example-img .
-docker run -it -p 443:443 -d --name v4h-example v4h-example-img
+docker run -it -p 80:80 -p 443:443 -d --name v4h-example v4h-example-img
