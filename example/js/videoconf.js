0faadedbc78f14718c9fb89d13f9e4f7c6c8ed84
Leoberto Soares <leobertosoares@lavid.ufpb.br>
2020-11-24 17:04:09 -0300

    add transcription

--- example/js/videoconf.js
+++ example/js/videoconf.js
@@ -16,12 +16,16 @@ function startVideoConf() {
 
   // Requisitar uma conferência
 	v4h_api.requestConference(sessionId).then(function (sessionId) {
-
+    
     // Desabilitar o botão para iniciar a conferência
     document.getElementById("insert-container").style.display = 'none';
 
     //Iniciar uma conferência
-    v4h_api.startConference(sessionId, document.querySelector('#meet'), '75%', '75%', 'Joãp Silva', 'https://picsum.photos/200').then(function() {
+    v4h_api.startConference(sessionId, document.querySelector('#meet'), '75%', '75%', 'João Silva', 'https://picsum.photos/200').then(function() {
+
+      runSpeechRecognition();
+
+      document.getElementById("output").style.display = 'block';
 
       v4h_api.jApi.executeCommand("toggleAudio");
 
@@ -43,7 +47,7 @@ function startVideoConf() {
     joinVideoConf();
 
   });
-  
+
 }
 
 // Funcão executada quando o botão de juntar-se a uma conferência é clicado
@@ -56,6 +60,10 @@ function joinVideoConf() {
   //Iniciar uma conferência
   v4h_api.joinConference(sessionId, document.querySelector('#meet'), '75%', '75%', 'José Silva', 'https://picsum.photos/200').catch(function() {
     
+    runSpeechRecognition();
+
+    document.getElementById("output").style.display = 'block';
+
     alert('Problema ao entrar na sessão: sessão indisponível. ');
     
     document.getElementById("insert-container").style.display = 'block';
@@ -81,4 +89,44 @@ function conferenceEnded(mySessionId) {
   
   document.getElementById("insert-container").style.display = 'block';
 
+  document.getElementById("output").style.display = 'none';
+
+}
+
+function runSpeechRecognition() {
+
+  // get output div reference
+  var output = document.getElementById("output");
+
+  // new speech recognition object
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+  recognition.lang = 'pt-BR';
+  recognition.interimResults = true;
+  recognition.continuous = true;
+  
+  // This runs when the speech recognition service starts
+  recognition.onsoundstart = function() {
+    console.log('ON SOUND START');
+    recognition.start();
+  };
+  
+  recognition.onspeechend = function() {
+    console.log('ON SPEACH END');
+    //recognition.stop();
+  }
+
+  // This runs when the speech recognition service returns result
+  recognition.onresult = function(event) {
+    var testes = Object.values(event.results);
+
+    testes.map((phrase) => {
+      output.innerHTML = "<b>Transccription:</b> " + phrase[0].transcript + "<br/> <b>Confidence:</b> " + phrase[0].confidence*100+"%";
+      console.log(phrase);
+    })
+
+  };
+
+    // start recognition
+    recognition.start();
 }
